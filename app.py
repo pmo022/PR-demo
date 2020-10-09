2aa5c6e10230b1a94db53c21308248f408840722
Patrick Monslaup <patrick.monslaup@gmail.com>
2020-10-09 10:25:22 +0200

    Update app.py
    Updated webpage documentation

--- app.py
+++ app.py
@@ -15,24 +15,55 @@ def index():
 
 def header():
     return f'''
-        <header>
-            Velkommen til krasjkurs i avansert git
-        </header>
-        <br/>
+        <h2>
+            Krasjkurs i code reviews/pull requests
+        </h2>
+        <p>
+            <b>Mål for kurset</b> er at vi skal lære branching, merging, og pull requests<br/>
+            Vi har laget en grunnleggende nettside, og ønsker å utvide den med enkel funksjonalitet
+        </p>
+        <p>
+            <strong>Ikke glad i terminal?</strong> <br/>
+            Det finnes mange gode grafiske verktøy som gjør git lettere,
+            f.eks. GitHub desktop, git kraken, sourcetree, osv.</br>
+        <p>
+        
+        <hr/>
     '''
 
 def body():
     return '''
-        <div class="body">
-            Mål for kurset er at vi skal lære branching, merging, og pull requests
+        <h3>Basics</h3>
+        <p>
+            Hvis du er ustø på grunnleggende git les en guide på internett etterpå.<br/>
+            Git er bransje-standard, og jeg vet mange av dere har brukt det en del allerede så jeg hopper over introduksjonen.<br/>
+            <br/>
+            <a href="https://www.atlassian.com/git/tutorials/what-is-version-control">Atlassian</a>
+            har en fantastisk guide, les denne.<br/>
+        </p>
 
-            Hvordan fungerer branching?<br/>
-            <a href="https://www.atlassian.com/git/tutorials/using-branches#:~:text=A%20branch%20represents%20an%20independent%20line%20of%20development.&text=The%20git%20branch%20command%20lets,checkout%20and%20git%20merge%20commands">Atlassian</a>
-            har en fantastisk guide, les denne. <br/>
-        </div>
+        <hr/>
+
+        <h3>Branching</h3>
+        <p>
+            <a href="https://www.atlassian.com/git/tutorials/using-branches">Atlassian</a>
+            har også en veldig fin guide her, les denne om du vil lære mer. <br/>
+        </p>
         <img src="https://wac-cdn.atlassian.com/dam/jcr:389059a7-214c-46a3-bc52-7781b4730301/hero.svg?cdnVersion=1287"/>
+        
+        <hr/>
         <br/>
-        <br/>
+
+        <h3>Merging</h3>
+        <p>
+            For å merge skriv `git merge <navn på branch>`.<br/>
+            En bedre måte å gjøre det på er ved å åpne pull requests
+        </p>
+        <img src="https://wac-cdn.atlassian.com/dam/jcr:86eba9ec-9391-45ea-800a-948cec1f2ed7/Branch-2.png?cdnVersion=1287"/>
+        <img src="https://wac-cdn.atlassian.com/dam/jcr:83323200-3c57-4c29-9b7e-e67e98745427/Branch-1.png?cdnVersion=1287"/>
+
+        
+        <hr/>
     '''
 
 def footer():
@@ -46,14 +77,7 @@ def footer():
 stylesheet = '''
     <style>
         body {
-            font-size: 20px;
-        }
-
-        header {
-            font-size: 1.4rem;
-            border-bottom: 1px solid black; 
-            margin-bottom: 30px;
-            padding-bottom: 10px;
+            font-size: 24px;
         }
 
         footer {
